a0a323e8af656f77fcec22e678bcdd69d95d2137
hiroki <m.maze.alc@gmail.com>
2024-09-08 18:37:45 +0900

    modified r104

--- starter/backend/app.py
+++ starter/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from resources import Movies
 
 app = Flask(__name__)
@@ -6,11 +6,6 @@ app = Flask(__name__)
 # Routes
 app.add_url_rule('/movies/', view_func=Movies.as_view('movies'))
 app.add_url_rule('/movies/<movie_id>', view_func=Movies.as_view('movie'))
-
-# Root endpoint
-@app.route('/')
-def index():
-    return jsonify(message="Welcome to the Movies API")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
